1c2b051a02bacddabcaf369bbdf4b98bd01db45f
nuleongee <nuleongee@gmail.com>
2022-02-25 23:59:05 +0900

    chore: Add tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,34 +2,44 @@
 module.exports = {
   pathPrefix: "/nuleonglog",
   siteMetadata: {
-      title: `nuleonlog`,
-    siteUrl: `https://nuleongee.github.io`
+    title: `nuleonlog`,
+    siteUrl: `https://nuleongee.github.io`,
   },
   plugins: [
-  //     {
-  //   resolve: 'gatsby-plugin-google-analytics',
-  //   options: {
-  //     "trackingId": ""
-  //   }
-  // },
-    "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    //     {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     "trackingId": ""
+    //   }
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    `gatsby-plugin-postcss`,
+  ],
 };
---
+++ src/pages/dev/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const DevIndex = () => {
+  return <main className="text-red-50">개발 정보 목록 노출</main>;
+};
+
+export default DevIndex;
