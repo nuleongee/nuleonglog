f433938980805e6718b352ca69bb48fcd477d0bb
nuleongee <nuleongee@gmail.com>
2022-02-26 23:09:08 +0900

    Updates

---
+++ component---src-pages-dev-index-tsx-21a1d3e8b3272335c36a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknuleonglog=self.webpackChunknuleonglog||[]).push([[862],{5783:function(e,l,n){n.r(l);var t=n(7294);l.default=function(){return t.createElement("main",null,t.createElement("h1",{className:"text-3xl font-bold underline"},"Hello world!"))}}}]);
+//# sourceMappingURL=component---src-pages-dev-index-tsx-21a1d3e8b3272335c36a.js.map
