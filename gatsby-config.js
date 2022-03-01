b3c08131f59ddec6fe0a5db709d186a30c601bb6
nuleongee <nuleongee@gmail.com>
2022-03-01 23:42:23 +0900

    feat: Add markdown post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,52 @@
-/** @type {import('gatsby).GatsbyConfig} */
 module.exports = {
-  pathPrefix: "/nuleonglog",
+  pathPrefix: '/nuleonglog',
   siteMetadata: {
     title: `nuleonlog`,
     siteUrl: `https://nuleongee.github.io`,
   },
   plugins: [
-    "gatsby-plugin-postcss",
     //     {
     //   resolve: 'gatsby-plugin-google-analytics',
     //   options: {
     //     "trackingId": ""
     //   }
     // },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    'gatsby-transformer-remark',
   ],
 };
